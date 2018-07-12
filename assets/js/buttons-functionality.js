6767be1cc755e8812bd897a9f261e1ab33279a99
Negin Namiranian <neginnamiranian@Negins-MacBook-Pro.local>
2018-07-12 12:18:40 -0700

    Code Cleaned up and commented

--- assets/js/buttons-functionality.js
+++ assets/js/buttons-functionality.js
@@ -2,41 +2,41 @@
 window.onload=function(){
 
     var intervalId;
+    //Adding event listener to our main buttons
     document.getElementById("add-button").addEventListener("click", addButtons);
     document.getElementById("clear-button").addEventListener("click", clear);
     document.getElementById("remove-button").addEventListener("click", removeSlowly);
     
-
+    //Defining a function that adds buttons dynamically
     function addButtons(){
         window.clearInterval(intervalId);
         for(i=0;i<40;i++){
             var btn= document.createElement('button');
             btn.setAttribute("id","Button "+(i+1));
             btn.setAttribute("class","added-button");
+            //Adding event listener to the dynamically made buttons
             btn.setAttribute("onclick","showAlert(this.id)");
             btn.innerText="Button "+(i+1);
             document.getElementById("button-section").appendChild(btn);
 
         }
     }
+    //Defining a function that clear all the buttons at once
     function clear(){
         window.clearInterval(intervalId);
         document.getElementById("button-section").innerHTML="";
     }
+    //Defining a function that removes the added buttons one by one
     function removeSlowly(){
         intervalId= window.setInterval(
             function () {
                 //finds the last button
                 var select=document.getElementById("button-section");
                 select.removeChild(select.lastChild);
-                //document.getElementById("button-section").removeChild();
-            }, 1000);
-    }
- 
-
- 
+            }, 1000);      
+    } 
 }
+//Alert function, triggers when any dynamically added button is clicked
 function showAlert(ID) {   
     alert(ID+" was clicked!");   
 }
-
